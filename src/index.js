e8a26fc08d24873f256b0d797ee040073c9c7075
ringosingson <ringosingson@live.com>
2019-03-11 15:12:26 +0530

    props-2 exercise

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+var USER_DATA = {
+  name: "Ringo Singson",
+  username: "ringosingson",
+  img:
+    "https://avatars1.githubusercontent.com/u/15993546?s=400&u=df3e33d3a1737531c7ebefe5c3b0fa4e971f4f5c&v=4"
+};
+
 class Badge extends React.Component {
   render() {
     return (
       <div>
-        <h1>
-          {this.props.name} <span>({this.props.username})</span>
-        </h1>
-        <img width="200px" height="200px" src={this.props.img} />
+        <img src={this.props.user.img} />
+        <h1>Name: {this.props.user.name}</h1>
+        <h3>Username: {this.props.user.username}</h3>
       </div>
     );
   }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Badge
-    name="Ringo Singson"
-    username="ringosingson"
-    img="https://avatars1.githubusercontent.com/u/15993546?s=400&u=df3e33d3a1737531c7ebefe5c3b0fa4e971f4f5c&v=4"
-  />,
-  rootElement
-);
+ReactDOM.render(<Badge user={USER_DATA} />, rootElement);
