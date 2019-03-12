89f5e77e8e165385030203141c325edb320dc753
ringosingson <ringosingson@live.com>
2019-03-12 15:36:11 +0530

    props-4 exercise

--- src/index.js
+++ src/index.js
@@ -3,44 +3,19 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-class Avatar extends React.Component {
+class Users extends React.Component {
   render() {
     return (
-      <img src={this.props.img}/>
-    )
+      <ul>
+        {this.props.list.map(function(name) {
+          return <li>{name}</li>;
+        })}
+      </ul>
+    );
   }
 }
 
-class Label extends React.Component {
-  render() {
-    return (
-      <h1>Name:{this.props.name} </h1>
-    )
-  }
-}
-
-class ScreenName extends React.Component {
-  render() {
-    return (
-      <h3>Username: {this.props.username}<h3/>
-    )
-      }
-    }
-    
-class Badge extends React.Component {
-          render() {
-        return(
-        <div>
-        <Avatar img = {this.props.user.img}/>
-        <Label name= {this.props.user.name}/>
-        <ScreenName username= {this.props.user.username}/>
-        </div>
-    )
-      }
-}
-
-ReactDOM.render(<Badge user = {{
-          name: "Ringo Singson",
-          username: "ringosingson",
-          img: "https://avatars1.githubusercontent.com/u/15993546?s=400&u=df3e33d3a1737531c7ebefe5c3b0fa4e971f4f5c&v=4",
-        }} />, document.getElementById("root");
+ReactDOM.render(
+  <Users list={["Ringo", "Tonga", "Monna", "Lenna"]} />,
+  document.getElementById("root")
+);
