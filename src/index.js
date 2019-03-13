db9e5d0783a39ff080988f614b6858ba4deeecb3
ringosingson <ringosingson@live.com>
2019-03-13 21:24:03 +0530

    props-5

--- src/index.js
+++ src/index.js
@@ -5,17 +5,46 @@ import "./styles.css";
 
 class Users extends React.Component {
   render() {
+    var friends = this.props.list.filter(function(user) {
+      return user.friend === true;
+    });
+
+    var nonFriends = this.props.list.filter(function(user) {
+      return user.friend !== true;
+    });
+
     return (
-      <ul>
-        {this.props.list.map(function(name) {
-          return <li>{name}</li>;
-        })}
-      </ul>
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function(user) {
+            return <li key={user.name}>{user.name}</li>;
+          })}
+        </ul>
+
+        <hr />
+
+        <h1>Non Friends</h1>
+        <ul>
+          {nonFriends.map(function(user) {
+            return <li key={user.name}>{user.name}</li>;
+          })}
+        </ul>
+      </div>
     );
   }
 }
-
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Users list={["Ringo", "Tonga", "Monna", "Lenna"]} />,
-  document.getElementById("root")
+  <Users
+    list={[
+      { name: "Ringo Singson", friend: true },
+      { name: "PAoa", friend: false },
+      { name: "Zoua", friend: true },
+      { name: "Monna", friend: false },
+      { name: "Lenna", friend: true },
+      { name: "Tonga", friend: false }
+    ]}
+  />,
+  rootElement
 );
